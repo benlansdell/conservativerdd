ff93eb6762378cd64af5d428d499aa7a7e3c1718
Ben Lansdell <lansdell@uw.edu>
2019-04-22 09:10:47 -0400

    Implement CLUCB

--- lib/bandits.py
+++ lib/bandits.py
@@ -2,9 +2,13 @@
 
 * LinUCB
 * ThresholdBandit 
+* ThresholdConsBandit
 * Greedy algorithm
+* Conservative LinUCB algorithm (Kazerouni et al 2017)
 """
+
 import numpy as np
+import numpy.random as rand
 
 def first_nonzero(arr, axis, invalid_val=-1):
     mask = arr!=0
@@ -63,11 +67,6 @@ class BanditAlgorithm(object):
 
 class LinUCB(BanditAlgorithm):
 
-	def arms(self, ctx, arm):
-		a = np.zeros(self.d*self.k)
-		a[int(arm)*self.d:((int(arm)+1)*self.d)] = ctx
-		return a
-
 	def __init__(self, generator, delta = 0.1, n_pulls = 10000, lambd = 1e-4):
 		L = 1
 		self.lamd = lambd*n_pulls
@@ -75,6 +74,14 @@ class LinUCB(BanditAlgorithm):
 		self.V = self.lamd*np.identity(generator.params.k*generator.params.d)
 		self.U = np.atleast_2d(np.ones(generator.params.k*generator.params.d)).T
 		super(LinUCB, self).__init__(generator, delta = delta, n_pulls = n_pulls)
+
+	def theta(self):
+		return np.dot(np.linalg.inv(self.V), self.U)
+
+	def arms(self, ctx, arm):
+		a = np.zeros(self.d*self.k)
+		a[int(arm)*self.d:((int(arm)+1)*self.d)] = ctx
+		return a
 
 	def _choose_arm(self, ctx):
 		theta = np.dot(np.linalg.inv(self.V), self.U)
@@ -224,3 +231,140 @@ class ThresholdConsBandit(ThresholdBandit):
 				#Conservative update
 				theta_k = theta_hat_k + beta_k*diff_k/norm_k
 			self.theta_tilde[idx,:] = np.squeeze(theta_k)
+
+class ConsLinUCB(BanditAlgorithm):
+	"""
+	Based on: Conservative contextual linear bandits
+
+	Kazerouni et al 2017
+
+	https://arxiv.org/pdf/1611.06426.pdf
+	"""
+
+	def __init__(self, generator, baseline_params, delta = 0.1, n_pulls = 10000, lambd = 1e-4, alpha = 0.1):
+		L = 1
+		self.alpha = alpha
+		self.baseline_alpha = baseline_params[0]
+		self.baseline_beta = baseline_params[1]
+		self.lamd = lambd*n_pulls
+		self.beta = lambda v: np.sqrt(self.lamd)*L + np.sqrt(np.log(np.linalg.det(v))-self.d*np.log(self.lamd)-2*np.log(delta))
+		self.V = self.lamd*np.identity(generator.params.k*generator.params.d)
+		self.U = np.atleast_2d(np.ones(generator.params.k*generator.params.d)).T
+		self.z = np.zeros(generator.params.d)
+		self.reward_baseline_cons = 0
+		self.reward_baseline_all = 0
+		super(ConsLinUCB, self).__init__(generator, delta = delta, n_pulls = n_pulls)
+
+	def theta(self):
+		return np.dot(np.linalg.inv(self.V), self.U)
+
+	def step(self):
+		if self.pull >= self.n_pulls:
+			print("Bandit: exceeded max pulls, returning Nones")
+			return [None]*4
+		ctx = self.generator.context()
+		opt_arm_idx = self._choose_opt_arm(ctx)
+		obs, regret, baseline_reward = self._play_baseline_arm(ctx)
+		self.reward_baseline_all += baseline_reward
+		L = self._compute_L(opt_arm_idx, ctx) + self.reward_baseline_cons
+		#Decide to play optimistic or baseline action
+		if L >= (1-self.alpha)*self.reward_baseline_all:
+			arm_idx = opt_arm_idx
+			obs, regret, _ = self.generator.pull(ctx,arm_idx)
+			self.contexts[self.pull,:] = ctx
+			self.arms_idx[self.pull] = arm_idx
+			self.obs[self.pull] = obs
+			self._update_bandit()
+		else:
+			arm_idx = -1
+			self.reward_baseline_cons += baseline_reward
+			self.contexts[self.pull,:] = ctx
+			self.arms_idx[self.pull] = arm_idx
+			self.obs[self.pull] = obs
+		self.pull += 1
+		return (ctx, arm_idx, obs, regret)
+
+	def _compute_L(self, opt_arm_idx, ctx):
+		L = 0
+		for idx in range(self.k):
+			#Compute z_t (the sum of all previous contexts where action k was played, plus the current context)
+			zt = np.sum(self.contexts[self.arms_idx == idx,:], axis = 0)
+			if idx == opt_arm_idx:
+				zt += ctx
+			L += self.predict_lower(zt, idx)
+		return L
+
+	def arms(self, ctx, arm):
+		a = np.zeros(self.d*self.k)
+		a[int(arm)*self.d:((int(arm)+1)*self.d)] = ctx
+		return a
+
+	def _play_baseline_arm(self, ctx):
+		eta = rand.randn()*self.generator.params.sigma_eps
+		expt_reward = np.dot(ctx[1:],self.baseline_alpha) + self.baseline_beta
+		obs = expt_reward + eta
+		#Compare w optimistic arms to get regret
+		ucbs = []
+		theta = np.dot(np.linalg.inv(self.V), self.U)
+		for arm in [self.arms(ctx, i) for i in range(self.k)]:
+			arm = np.atleast_2d(arm).T
+			ucb = np.dot(theta.T, arm)
+			ucbs.append(ucb[0][0])
+		ucbs.append(expt_reward)
+		regret = max(ucbs) - expt_reward
+		return obs, regret, expt_reward
+
+	def _choose_opt_arm(self, ctx):
+		theta = np.dot(np.linalg.inv(self.V), self.U)
+		ucbs = []
+		for arm in [self.arms(ctx, i) for i in range(self.k)]:
+			arm = np.atleast_2d(arm).T
+			ucb = np.dot(theta.T, arm)
+			ucb += self.beta(self.V)*np.sqrt(np.dot(arm.T, np.dot(np.linalg.inv(self.V), arm)))
+			ucbs.append(ucb[0][0])
+		return ucbs.index(max(ucbs))
+
+	def _update_bandit(self):
+		ctx = self.contexts[self.pull]
+		arm = self.arms(ctx, self.arms_idx[self.pull])
+		arm = np.atleast_2d(arm).T
+		obs = self.obs[self.pull]
+		self.V += np.dot(arm, arm.T)
+		self.U += obs*arm
+
+	def predict(self, ctx, arm_idx):
+		arm = self.arms(ctx, arm_idx)
+		arm = np.atleast_2d(arm).T
+		theta = np.dot(np.linalg.inv(self.V), self.U)
+		pred = np.dot(theta.T, arm)
+		return pred
+
+	def predict_upper(self, ctx, arm_idx):
+		arm = self.arms(ctx, arm_idx)
+		arm = np.atleast_2d(arm).T
+		theta = np.dot(np.linalg.inv(self.V), self.U)
+		pred = np.dot(theta.T, arm)
+		pred += self.beta(self.V)*np.sqrt(np.dot(arm.T, np.dot(np.linalg.inv(self.V), arm)))
+		return pred
+
+	def predict_lower(self, ctx, arm_idx):
+		arm = self.arms(ctx, arm_idx)
+		arm = np.atleast_2d(arm).T
+		theta = np.dot(np.linalg.inv(self.V), self.U)
+		pred = np.dot(theta.T, arm)
+		pred -= self.beta(self.V)*np.sqrt(np.dot(arm.T, np.dot(np.linalg.inv(self.V), arm)))
+		return pred
+
+	def pred_arm(self, arm_idx, N = 100):
+		xvals = np.linspace(0, 1, N)
+		upper = np.zeros(N)
+		lower = np.zeros(N)
+		mean = np.zeros(N)
+		for ctx in xvals:
+			arm = self.arms(ctx, arm_idx)
+			arm = np.atleast_2d(arm).T
+			theta = np.dot(np.linalg.inv(self.V), self.U)
+			pred = np.dot(theta.T, arm)
+			lower = pred - self.beta(self.V)*np.sqrt(np.dot(arm.T, np.dot(np.linalg.inv(self.V), arm)))
+			upper = pred + self.beta(self.V)*np.sqrt(np.dot(arm.T, np.dot(np.linalg.inv(self.V), arm)))
+		return pred, lower, upper
--- run_bandits.py
+++ run_bandits.py
@@ -1,20 +1,19 @@
-from lib.bandits import LinUCB, ThresholdBandit, ThresholdConsBandit, GreedyBandit, expected_regret
+from lib.bandits import LinUCB, ThresholdBandit, ThresholdConsBandit, GreedyBandit, ConsLinUCB, expected_regret
 from lib.generator import LinearGeneratorParams, LinearGenerator
 import numpy as np 
 from scipy.stats import truncnorm
 
-M = 30    #number of runs
-N = 10000 #number of timesteps
 #alg = 'greedy'
 #alg = 'linucb'
 #alg = 'threshold'
-alg = 'thresholdcons'
+#alg = 'thresholdcons'
+alg = 'conslinucb'
 
-save = True
-
+M = 30    #number of runs
+N = 10000 #number of timesteps
+save = False
 max_alpha = 2
 max_beta = 2
-
 k = 2    #Number of arms
 d = 5   #Dimension of context (includes one dim for intercept term)
 intercept = True
@@ -28,6 +27,8 @@ elif alg == 'threshold':
 	BanditAlg = ThresholdBandit
 elif alg == 'thresholdcons':
 	BanditAlg = ThresholdConsBandit
+elif alg == 'conslinucb':
+	BanditAlg = ConsLinUCB
 else:
 	print "Select a valid algorithm"
 
@@ -35,9 +36,12 @@ else:
 alphas = truncnorm.rvs(-max_alpha, max_alpha, scale = 1, size=(M,k,d-1))
 betas = truncnorm.rvs(-max_beta, max_beta, scale = 1, size=(M,k))
 
+baseline_alphas = truncnorm.rvs(-max_alpha, max_alpha, scale = 1, size=(M,1,d-1))
+baseline_betas = truncnorm.rvs(-max_beta, max_beta, scale = 1, size=(M,1))
+
 regret = np.zeros((M, N))
 expt_regret = np.zeros((M, N))
-arm_pulls = np.zeros((M, N, k))
+arm_pulls = np.zeros((M, N, k+1))
 n_changes = np.zeros((M, N))
 update_pol = np.zeros((M, N))
 
@@ -47,22 +51,23 @@ fn_out = './runs/bandit_%s_M_%d_N_%d.npz'%(alg, M, N)
 for m in range(M):
 	params = LinearGeneratorParams(np.atleast_2d(alphas[m,:,:]), betas[m,:], d = d, k = k, intercept = intercept)
 	generator = LinearGenerator(params)
-	bandit = BanditAlg(generator)
-	
+	bandit = BanditAlg(generator, (np.squeeze(baseline_alphas[m,:]), baseline_betas[m,0]))
 	print("Run: %d/%d"%(m+1,M))
 	for i in range(N):
 		(ctx, arm_idx, obs, r) = bandit.step()
 		#print((arm_idx, obs, r))
 		regret[m,i] = r
-		arm_pulls[m,i,arm_idx] = 1
+		if arm_idx >= 0:
+			arm_pulls[m,i,arm_idx] = 1
+		else:
+			arm_pulls[m,i,k] = 1			
 		#if i % evaluate_every == 1:
 		#	expt_reg[m,i] = expected_regret(bandit, generator)
-		if bandit.update_theta[bandit.pull-1] == 1:
-			ereg = expected_regret(bandit, generator)
-			print("Expected regret is now: %f"%ereg)
-			expt_regret[m,i] = ereg
-
-	update_pol[m,:] = bandit.update_theta
+		#if bandit.update_theta[bandit.pull-1] == 1:
+		#	ereg = expected_regret(bandit, generator)
+		#	print("Expected regret is now: %f"%ereg)
+		#	expt_regret[m,i] = ereg
+	#update_pol[m,:] = bandit.update_theta
 
 if save:
 	np.savez(fn_out, regret = regret, arm_pulls = arm_pulls, update_pol = update_pol, expt_reg = expt_reg)
